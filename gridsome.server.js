03730730a9c41806082e22c988f00553eba85174
thilak <59470373+thilakm-git@users.noreply.github.com>
2022-08-18 17:40:51 +0200

    added function config to allow programmatic dynamic routes

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,3 +4,13 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = function (api) {
+    api.loadSource(({ addCollection }) => {
+      // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    })
+  
+    api.createPages(({ createPage }) => {
+      // Use the Pages API here: https://gridsome.org/docs/pages-api/
+    })
+  }
