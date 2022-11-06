bcaed82b20960ee53720147fe786d82048b7168c
thilak <59470373+thilakm-git@users.noreply.github.com>
2022-11-06 19:47:23 +0100

    use absolute paths

--- gridsome.config.js
+++ gridsome.config.js
@@ -18,6 +18,7 @@ module.exports = {
       options: {
         typeName: 'Themenkatalog',
         path: 'content/catalogue/*.md',
+        resolveAbsolutePaths: true,
         refs: {
           tags: {
             typeName: 'Tag',
